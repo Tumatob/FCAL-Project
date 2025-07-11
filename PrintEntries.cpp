f6ebfd78d0d9d2efdfaa008ef36e2c6484ce2362
Kevin Zheng <kzheng@ifarm2402.jlab.org>
2025-07-11 14:36:10 -0400

    Created multiple graphs to loop through specific columns and filled onto a canvas

--- PrintEntries.cpp
+++ PrintEntries.cpp
@@ -12,7 +12,11 @@ void PrintEntries(const char* filename) {
     //Declare varaibles to hold branch data
     UInt_t channelNum, eventNum, rocid, slot, channel, itrigger, peak_raw, integral, pedestal, time, nsamples, QF, row, column, fptrigger;
     float peak;
-
+    
+    //Removes legend from all histograms
+    gStyle->SetOptStat(0);
+    
+    
     //Set Branch Address
     t->SetBranchAddress("channelnum", &channelNum);
     t->SetBranchAddress("eventnum", &eventNum);
@@ -47,25 +51,48 @@ void PrintEntries(const char* filename) {
     std::vector<int> HV_Values = {1400, 1450 ,1500 ,1550, 1600, 1650, 1700, 1750, 1800};
     TH2F* hvledComparison = new TH2F("hvledComparison", "Avg Peak: LED vs HV", 28, 1.5, 29.5, 9, 1375, 1825);
     
+    //Assign the row and specific column being looked at
+    int fixedRow = 29;
+    //The columns that need to be looked at
+    int colMin = 0;
+    int colMax = 29;
+    int colMin2 = 30;
+    int colMax2 = 58;
+    //Number of columns being analyzed
+    int nCols2 = colMax2 - colMin2 + 1;
+    int nCols = colMax - colMin + 1; 
     
-    int fixedRow = 10;
-    int colMin   = 0;
-    int colMax   = 10;
-    int nCols    = colMax - colMin + 1; 
+    //Define two standard vecotrs 
     std::vector<std::vector<int>> full_peak_col(nCols, std::vector<int>(2520, 0));
+    std::vector<std::vector<int>> full_peak_col2(nCols2, std::vector<int>(2520, 0));
+    
+    //Assign a pointer 
     std::vector<TH2F*>    hHVLED(nCols);
     std::vector<TCanvas*> cHVLED(nCols);
-
-
-    for (int col = colMin; col <= colMax; col++) { 
-    	int idx = col - colMin; 
-
-    	hHVLED[idx] = new TH2F(Form("hHVLED_col%d", col),
-    	Form("Avg Peak: LED vs HV (Row %d, Col %d);LED Value;HV Value", fixedRow, col), 28, 1.5, 29.5, 9,1375,1825);
+    std::vector<TH2F*>    hHVLED2(nCols2);
+    std::vector<TCanvas*> cHVLED2(nCols2);
+    
+    
+    //Loops through 0-29 to create histograms
+    for (int i = 0; i <= colMax; i++) { 
+    	
+    	//Create a new TH2F and storing it into hHVLED
+    	hHVLED[i] = new TH2F(Form("hHVLED_col%d", i),
+    	Form("Avg Peak: LED vs HV (Row %d, Col %d);LED Value;HV Value", fixedRow, i), 28, 1.5, 29.5, 9, 1375, 1825);
     }
-
-
-    for (long i = 0; i < entries; ++i) {
+    
+    //Loops through 30-58 to create histograms
+    for (int i = 0; i < nCols2; i++) {
+    	int colNumber = i + colMin2;
+    	
+    	hHVLED2[i] = new TH2F(Form("hHVLED_col%d", colNumber),
+        Form("Avg Peak: LED vs HV (Row %d, Col %d);LED Value;HV Value", fixedRow, colNumber), 28, 1.5, 29.5, 9, 1375, 1825);
+    }
+    
+    
+    
+    //Loop through all the entries in the run
+    for (long i = 0; i < entries; i++) {
         t->GetEntry(i);
         
         /* if (eventNum == 2000) {
@@ -73,24 +100,36 @@ void PrintEntries(const char* filename) {
     	} */
 
 	//Takes data from one block in the calorimeter
-        if (row == fixedRow && column <= colMax) {
-    	
-    		peak_raw_val.push_back(peak_raw);
-    		eventNo.push_back(fptrigger);
-    		int idx = column - colMin;
-    		full_peak_col[idx][fptrigger] = peak_raw;
+	
+	
+	//Loops through the row
+	if (row == fixedRow) {
+	
+    		/* peak_raw_val.push_back(peak_raw);
+    		eventNo.push_back(fptrigger); */
+    		
+    		
+    		//Loops through the two sets of columns and pushes back the fptrigger in location of full_peak_col
+    		if (column >= colMin && column <= colMax) {
+    		
+    			full_peak_col[column - colMin][fptrigger] = peak_raw;
+    			
+    		}
+    		else if (column >= colMin2 && column <= colMax2) {
+    		
+        		full_peak_col2[column - colMin2][fptrigger] = peak_raw;
+        		
+        	}
     		/* std::cout << "fptrigger: " << fptrigger << " Peak Raw: " << peak_raw << std::endl; */
+    		
+    		/* bottom_integral->Fill(integral);
+    		bottom_time->Fill(time);
+    		bottom_peak->Fill(peak_raw); */
+    		
+    	}
     	  
-          	/* bottom_integral->Fill(integral);
-          	bottom_time->Fill(time);
-          	bottom_peak->Fill(peak_raw); */
-        }
-        //Print all values
-        /* std::cout << peak << std::endl; */
-
-    }
-    
-
+     }
+     
 
     //Initialie an integer called entry_location
     int entry_location = 0;
@@ -101,14 +140,14 @@ void PrintEntries(const char* filename) {
             int sum = 0;
             int count_nonzero = 0;
 
-            for (int i = 0; i < 10; ++i) {
+            for (int i = 0; i < 10; i++) {
             
                 int val = full_peak[entry_location];
                 values.push_back(val);
                 if (val != 0) { 
                 	sum += val; ++count_nonzero; 
                 }
-                ++entry_location;
+                entry_location++;
             }
 
             int avg_int;
@@ -123,6 +162,7 @@ void PrintEntries(const char* filename) {
             hvledComparison->Fill(led, hv, avg_int);
         }
     }
+    
     
     //Fill histogram with data on all entries in the calorimeter
     /* for (long i = 0; i < entries; i++) {
@@ -139,41 +179,62 @@ void PrintEntries(const char* filename) {
     
     
     
-    int TriggersPerPoint = 10;
 
+    for (int i = 0; i <= colMax; i++) {	
+
+    	std::size_t pos = 0;
+
+    	for (int hv : HV_Values) {
+        	for (int led : LED_Values) {
+            		int sum = 0;
+            		int count_nonzero = 0;
+
+           	 	for (int trg = 0; trg < 10; trg++, pos++) {
+                		int val = full_peak_col[i][pos];
+                		if (val != 0) { sum += val; count_nonzero++; }
+            	}
+
+            	int avg_int;
+            	if (count_nonzero > 0) {
+                	avg_int = sum / count_nonzero;
+            	} else {
+                	avg_int = 0;
+            	}
+
+            	hHVLED[i]->Fill(led, hv, avg_int);
+        	}
+    	}
+    }
     
-    for (int col = colMin; col <= colMax; ++col) {
-    int colIdx   = col - colMin;
-    auto& colBuf = full_peak_col[colIdx];
+    
+    for (int i = 0; i < nCols2; i++) {
+    
+    	std::size_t pos = 0;
+    	
+    	for (int hv : HV_Values) {
+        	for (int led : LED_Values) {
+            		int sum = 0;
+            		int count_nonzero = 0;
 
-
-    std::size_t pos = 0;
-
-    for (int hv : HV_Values) {
-        for (int led : LED_Values) {
-            int sum = 0;
-            int good = 0;
-
-            for (int trg = 0; trg < TriggersPerPoint; ++trg, ++pos)
-            {
-                int val = colBuf[pos];
-                if (val != 0) { sum += val; ++good; }
-            }
-
-            int avg_int;
-            if (good > 0)
-            {
-    		avg_int = sum / good;
-    	    }
-    	    else
-    	    {
-    	    	avg_int = 0;
-    	    }
-    	    
-            hHVLED[colIdx]->Fill(led, hv, avg_int);
-        }
+            		for (int trg = 0; trg < 10; trg++, pos++) {
+                		int val = full_peak_col2[i][pos];
+                		if (val != 0) { sum += val; count_nonzero++; }
+            		}
+            
+            	int avg_int;
+            	if (count_nonzero > 0) {
+                	avg_int = sum / count_nonzero;
+            	} else {
+                	avg_int = 0;
+           
+           	}
+           	
+           	hHVLED2[i]->Fill(led, hv, avg_int);
+        	}
+    	}
     }
-    }
+
+
     
     //Creates a 2D histogram to display calorimeter
     /* TCanvas* c1 = new TCanvas("c1", "Row vs Column", 1000, 1000);
@@ -218,31 +279,60 @@ void PrintEntries(const char* filename) {
     bottom_peak->Draw("COLZ"); */
 
     //Creates a plot with peak_raw_val and loops over the event numbers
-    TGraph* graph = new TGraph(eventNo.size(), &eventNo[0], &peak_raw_val[0]);
-    graph->Draw("AP");
+    /* TCanvas* c7_canvas = new TCanvas("c7_canvas", "HVLED Comparison", 800, 600);
+    TGraph* c7 = new TGraph(eventNo.size(), &eventNo[0], &peak_raw_val[0]);
+    c7->Draw("AP"); */
     
     //Creates 1D histogram graph of values at one entry
-    /* TCanvas* c7 = new TCanvas("c7", "Map of One Entry", 800, 800);
+    /* TCanvas* c8 = new TCanvas("c8", "Map of One Entry", 800, 800);
     oneEntry->GetXaxis()->SetTitle("Row");
     oneEntry->GetYaxis()->SetTitle("Column");
     oneEntry->Draw("COLZ"); */
 
-    TCanvas* c8 = new TCanvas("c8", "Peak_raw per LED and HV", 800, 800);
+    /* TCanvas* c9 = new TCanvas("c9", "Peak_raw per LED and HV", 800, 800);
     hvledComparison->GetXaxis()->SetTitle("LED Value");
     hvledComparison->GetYaxis()->SetTitle("HV Value");
-    hvledComparison->Draw("COLZ");
+    hvledComparison->Draw("COLZ"); */
 
 
-    for (int col = colMin; col <= colMax; ++col) {
-        int idx = col - colMin;
-        
-        cHVLED[idx] = new TCanvas(Form("cHVLED_col%d", col), Form("Peak_raw per LED and HV – Row %d, Col %d", fixedRow, col), 800, 800);
-        hHVLED[idx]->Draw("COLZ");
+    /* for (int i = 0; i < nCols; ++i) {
+    
+    	cHVLED[i] = new TCanvas(Form("cHVLED_col%d", i),
+    	Form("Peak_raw per LED and HV – Row %d, Col %d", fixedRow, i), 800, 800);
+    	hHVLED[i]->Draw("COLZ"); 
+    } */
+    
+   //Create a canvas that stores histograms from column 0 - 29
+   TCanvas *cAll = new TCanvas("cAll", "All Column Histograms 0-29", 1600, 1000);
+   int rows = 6;
+   int cols = (nCols + 1) / 6;
+   cAll->Divide(cols, rows);
+    
+   for (int i = 0; i < nCols; i++) {
+   	cAll->cd(i+1);
+    	hHVLED[i]->Draw("COLZ");
     }
     
+    cAll->Update();
+    
+    //Create a canvas that stores histograms from column 30 - 58
+    TCanvas *cAll2 = new TCanvas("cAll2", "All Column Histograms 30–58", 1600, 1000);
+    int cols2 = 6;
+    int rows2 = (nCols2 + cols2 - 1) / cols2;
+    cAll2->Divide(cols2, rows2);
+    
+    for (int i = 0; i < nCols2; i++) {
+    	cAll2->cd(i+1);
+    	hHVLED2[i]->Draw("COLZ");
+    }
+    
+    cAll2->Update();
+
+
     
     //Print the total entries in the data
     std::cout << "Total Entries: " << entries << std::endl;
     /* std::cout << "Total Entries in peak_raw: " << peak_raw_val.size() << std::endl; */
+     
 }
 
